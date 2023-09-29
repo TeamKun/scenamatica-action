14f6274bd7614eb4520d7436b620648447e2183a
PeyaPeyaPeyang <peyang@peya.tokyo>
2023-09-29 19:29:33 +0900

    chore: Bump scenamatica version from 0.7.0 to 0.8.0

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ import * as core from "@actions/core"
 import type {PacketTestEnd} from "./packets";
 import {TestResultCause} from "./packets";
 
-const DEFAULT_SCENAMATICA_VERSION = "0.7.0"
+const DEFAULT_SCENAMATICA_VERSION = "0.8.0"
 const ENV_NO_SCENAMATICA = "NO_SCENAMATICA"
 
 const extractTestResults = (results: PacketTestEnd[]) => {
