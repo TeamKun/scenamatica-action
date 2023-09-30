36d0a5b221ab7ab3ae89ea0d7bad89e1f2fbb97a
PeyaPeyaPeyang <peyang@peya.tokyo>
2023-09-30 22:36:03 +0900

    fix: Invalid duration

--- src/dist/index.js
+++ src/dist/index.js
@@ -69213,10 +69213,13 @@ ${pieChart}
       const title = "Scenamatica Test Timeline";
       const dateFormat = "HH:mm:ss.SSS";
       const axisFormat = "%M:%S";
+      let lastFinishedAt = 0;
       const results = result.results.sort((a, b) => a.startedAt - b.startedAt).map((test) => {
-        const startedAt = toMermaidTime(test.startedAt);
-        const finishedAt = toMermaidTime(test.finishedAt);
+        const startedAt = toMermaidTime(lastFinishedAt);
+        const duration = test.finishedAt - test.startedAt;
+        const finishedAt = toMermaidTime(duration);
         const cause = causeToMermaidStatus(test.cause);
+        lastFinishedAt = duration;
         return `${test.scenario.name}: ${cause} ${startedAt}, ${finishedAt}`;
       });
       return `
